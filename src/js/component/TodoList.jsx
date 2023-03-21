a3d8662e3a004926ef2a87d43531e7474e8c71b4
Marione Fiszman <marionef1985@gmail.com>
2023-03-21 17:27:26 +0000

    formato al codigo, hover button

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -11,48 +10,48 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   return (
     <div className="outsideContainer">
-<div className="container">
-      <h1>my to-do's</h1>
-      <ul>
-        <li>
-          <input
-          className="input"
-            type="text"
-            onChange={(e) => {
-              setInputValue(e.target.value);
-            }}
-            value={inputValue}
-            onKeyDown={(e) => {
-              console.log("key was pressed", e.key);
-              if (e.key === "Enter") {
-                console.log("Enter was pressed");
-                setTodos(todos.concat(inputValue));
-                setInputValue("");
-              }
-            }}
-            placeholder="What do you need to do?"
-          ></input>
-        </li>
-        {todos.map((item, index) => (
-          <li key={index}>
-            <div className="listItems">{item}</div>{" "}
-            <button
-              onClick={() => {
-                setTodos(todos.filter((item,itemIndex) => {
-                  return itemIndex !==index;
-                }
-                ));
+      <div className="container">
+        <h1>my to-do's</h1>
+        <ul>
+          <li>
+            <input
+              className="input"
+              type="text"
+              onChange={(e) => {
+                setInputValue(e.target.value);
               }}
-            >
-              <i className="fa-solid fa-xmark"></i>
-            </button>
+              value={inputValue}
+              onKeyDown={(e) => {
+                console.log("key was pressed", e.key);
+                if (e.key === "Enter") {
+                  console.log("Enter was pressed");
+                  setTodos(todos.concat(inputValue));
+                  setInputValue("");
+                }
+              }}
+              placeholder="What do you need to do?"
+            ></input>
           </li>
-        ))}
-      </ul>
-      <div className="taskCounter">{todos.length} tasks left!</div>
+          {todos.map((item, index) => (
+            <li key={index}>
+              <div className="listItems">{item}</div>{" "}
+              <button
+                onClick={() => {
+                  setTodos(
+                    todos.filter((item, itemIndex) => {
+                      return itemIndex !== index;
+                    })
+                  );
+                }}
+              >
+                <i className="fa-solid fa-xmark"></i>
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div className="taskCounter">{todos.length} tasks left!</div>
+      </div>
     </div>
-    </div>
-    
   );
 };
 export default TodoList;
